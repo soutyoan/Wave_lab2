5567497be00815dd3448ad23669103498e973dd8
Yoan Souty <Yoan.Souty@grenoble-inp.org>
2018-11-30 16:26:41 +0100

    Better with the pdf hihi

--- compress_function.m
+++
@@ -1,9 +0,0 @@
-function compress_function(y)
-% Compress a given signal values y (must be length with power of 2) and 
-% plots 
-%   - 1D original plot
-%   - 1D wavelets coefficients
-%   - 1D reconstructed function
-n=length(y);
-
-end
